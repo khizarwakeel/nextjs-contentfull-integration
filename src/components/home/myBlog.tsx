3bb5fdcb52a876ac27c367cb1cbe3447c9628004
Khizar Wakeel <khizarwakeel@gmail.com>
2024-06-12 21:20:49 +0500

    added blog integration and some adjustment

--- src/components/home/myBlog.tsx
+++ src/components/home/myBlog.tsx
@@ -1,3 +1,6 @@
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Image from "next/image";
+
 const getBlogsData = async () => {
   const URL = `https://cdn.contentful.com/spaces/756bo1qps4ce/entries?access_token=e2gAdpdzZunrg4ryxiqckuZRfbuMowVHbq8gNjpO59g&content_type=blog`;
   try {
@@ -17,8 +20,24 @@ const getBlogsData = async () => {
 const MyBlog = async () => {
   const blogDataCont = await getBlogsData();
   console.log(blogDataCont);
+  const imageUrl = blogDataCont.includes.Asset[0].fields.file.url;
+  const imageWidth = blogDataCont.includes.Asset[0].fields.file.details.image.width;
+  const imageHeight = blogDataCont.includes.Asset[0].fields.file.details.image.height;
 
-  return <>{blogDataCont.items[0].fields.title}</>;
+  return (
+    <>
+      <h1>{blogDataCont.items[0].fields.title}</h1>
+      <Image
+        src={`https:${imageUrl}`}
+        width={imageWidth}
+        height={imageHeight}
+        alt="Blog Image"
+        title="Blog Image"
+        className="w-[60%] h-[60%] flex justify-center"
+      />
+      <div className="prose">{documentToReactComponents(blogDataCont.items[0].fields.body)}</div>
+    </>
+  );
 };
 
 export default MyBlog;
